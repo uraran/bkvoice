831e3eefbc78bbb534b1bf7b37b4aff0d9479095
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-07-31 16:27:13 +0000

    改用ArrayList<T>, 从电脑发送声音到手机有时OK,还需检验！

--- android/src/com/bktalk/BkvoiceActivity.java
+++ android/src/com/bktalk/BkvoiceActivity.java
@@ -8,6 +8,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.media.AudioFormat;
@@ -17,29 +18,43 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class BkvoiceActivity extends Activity {
-	final CircularBuffer<byte[]> cBuffer = new CircularBuffer<byte[]>(2048);  //环形缓冲区
-
+	//CircularBuffer cBuffer = null;
+	ArrayList<byte[]> buffer = null;
 	static final int frequency = 16000;
 	static final int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
 	static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
 	AudioTrack audioTrack;
 	int miniPlayBufSize;
 	
-	private static final String AudioName = "/sdcard/recoder.pcm";  
+	private static final String AudioRecvName = "/sdcard/recoder_recv.pcm";  
+	private static final String AudioPlayName = "/sdcard/recoder_play.pcm";  
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+        buffer = new ArrayList<byte[]>();
+        //cBuffer = new CircularBuffer(16);  //环形缓冲区
         (new Thread(new UDPRecvThread())).start();
         (new Thread(new AudioPlayThread())).start();
     }
     
     public class AudioPlayThread implements Runnable
     {
+    	FileOutputStream fos = null;
+    	
 		@Override
 		public void run() {
+/*
+			File file = new File(AudioPlayName);  
+			
+			try {
+				fos = new FileOutputStream(file);
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+*/
 			miniPlayBufSize = AudioTrack.getMinBufferSize(frequency,
 					channelConfiguration, audioEncoding);
 			
@@ -54,8 +69,19 @@ public class BkvoiceActivity extends Activity {
 			audioTrack.play(); 
 			
 			while(true)
-			{
-				audioTrack.write(cBuffer.getElement(), 0, 160);
+			{			/*	
+				try {
+					fos.write(buffer.get(0), 0, 160);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				*/
+				if(buffer.size() > 0)
+				{
+					audioTrack.write(buffer.get(0), 0, 160);
+					buffer.remove(0);
+				}
 			}
 		}
     	
@@ -69,11 +95,11 @@ public class BkvoiceActivity extends Activity {
 			Integer port = 8302;
 			InetAddress remoteAddress;
 			byte sendbuff[] = new byte[61];
-			byte rcvbuff[] = new byte[1024];
+			byte rcvbuff[] = new byte[160];
 			
 			FileOutputStream fos = null;
 			
-			File file = new File(AudioName);  
+			File file = new File(AudioRecvName);  
 			
 			try {
 				fos = new FileOutputStream(file);
@@ -82,6 +108,7 @@ public class BkvoiceActivity extends Activity {
 				e1.printStackTrace();
 			}
 			
+			
 			DatagramSocket datagramSocket = null;
 			try {
 				datagramSocket = new DatagramSocket(port);
@@ -89,6 +116,7 @@ public class BkvoiceActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
 			DatagramPacket  datagramPacket = new DatagramPacket(sendbuff, sendbuff.length);
 			DatagramPacket  rcvPacket = new DatagramPacket(rcvbuff, rcvbuff.length);
 			
@@ -96,13 +124,14 @@ public class BkvoiceActivity extends Activity {
 			{
 				try {
 					datagramSocket.receive(rcvPacket);
+					buffer.add(rcvbuff);
 					fos.write(rcvbuff, 0, rcvPacket.getLength());
-					cBuffer.putElement(rcvbuff);
+					//cBuffer.putElement(rcvbuff);
 					//Log.i("元素数量", String.valueOf(cBuffer.capacity));
-					Log.i("收到数据", String.valueOf(rcvPacket.getLength())); 
-				} catch (IOException e) {
+					//Log.i("收到数据", String.valueOf(rcvPacket.getLength())); 
+				} catch (IOException e1) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					e1.printStackTrace();
 				}
 			}
 			/*
