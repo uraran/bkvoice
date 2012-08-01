60f38c875a3557ed4c639478b990f915cc1501c3
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-08-01 03:58:17 +0000

    终于可以出声音了，很好很强大

--- android/src/com/bktalk/BkvoiceActivity.java
+++ android/src/com/bktalk/BkvoiceActivity.java
@@ -9,6 +9,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 import android.app.Activity;
 import android.media.AudioFormat;
@@ -26,35 +27,89 @@ public class BkvoiceActivity extends Activity {
 	AudioTrack audioTrack;
 	int miniPlayBufSize;
 	
+	final Semaphore semp = new Semaphore(5);  
+	
 	private static final String AudioRecvName = "/sdcard/recoder_recv.pcm";  
-	private static final String AudioPlayName = "/sdcard/recoder_play.pcm";  
+	private static final String AudioPlayName = "/sdcard/recoder_play_111.pcm";  
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         buffer = new ArrayList<byte[]>();
+        //(new Thread(new TestThread())).start();
         //cBuffer = new CircularBuffer(16);  //环形缓冲区
         (new Thread(new UDPRecvThread())).start();
         (new Thread(new AudioPlayThread())).start();
     }
     
+    public class TestThread implements Runnable
+    {
+		@Override
+		public void run() {
+			ArrayList<byte[]> list = new ArrayList<byte[]>(0);
+			byte[] data = new byte[20];
+			data[0] = 0;
+			data[1] = -100;
+			data[2] = (byte) -200;
+			data[3] = -100;
+			data[4] = -100;
+			list.add(data);
+			
+			data = new byte[20];
+			data[0] = 1;
+			list.add(data);
+			
+			data = new byte[20];
+			data[0] = 2;
+			list.add(data);
+			
+			data = new byte[20];
+			data[0] = 3;
+			list.add(data);
+			
+			data = new byte[20];
+			data[0] = 4;
+			list.add(data);
+			
+			byte[] dataread = list.get(0);
+			list.remove(0);
+			Log.i("test", String.valueOf(dataread.length));
+			
+			dataread = list.get(0);
+			list.remove(0);
+			Log.i("test", String.valueOf(dataread.length));
+			
+			dataread = list.get(0);
+			list.remove(0);
+			Log.i("test", String.valueOf(dataread.length));
+			
+			dataread = list.get(0);
+			list.remove(0);
+			Log.i("test", String.valueOf(dataread.length));
+			
+			dataread = list.get(0);
+			list.remove(0);
+			Log.i("test", String.valueOf(dataread.length));
+		}
+    	
+    }
     public class AudioPlayThread implements Runnable
     {
-    	FileOutputStream fos = null;
+    	FileOutputStream fosplay = null;
     	
 		@Override
 		public void run() {
-/*
+
 			File file = new File(AudioPlayName);  
 			
 			try {
-				fos = new FileOutputStream(file);
+				fosplay = new FileOutputStream(file);
 			} catch (FileNotFoundException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-*/
+
 			miniPlayBufSize = AudioTrack.getMinBufferSize(frequency,
 					channelConfiguration, audioEncoding);
 			
@@ -69,18 +124,44 @@ public class BkvoiceActivity extends Activity {
 			audioTrack.play(); 
 			
 			while(true)
-			{			/*	
-				try {
-					fos.write(buffer.get(0), 0, 160);
-				} catch (IOException e) {
+			{	
+				// 获取许可  
+                try {
+					semp.acquire();
+				} catch (InterruptedException e1) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				*/
-				if(buffer.size() > 0)
+					e1.printStackTrace();
+				}  
+                
+				if(buffer.size() > 5)
 				{
-					audioTrack.write(buffer.get(0), 0, 160);
+					//Log.i("play", String.valueOf(buffer.size()));
+					//int length = buffer.get(0).length;
+					byte[] data = buffer.get(0);
+					if(data != null)
+					{
+						audioTrack.write(data, 0, data.length);
+						
+						/*
+						try {
+							fosplay.write(data, 0, data.length);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						*/
+						
+					}
 					buffer.remove(0);
+				}
+				else
+				{
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		}
@@ -121,11 +202,21 @@ public class BkvoiceActivity extends Activity {
 			DatagramPacket  rcvPacket = new DatagramPacket(rcvbuff, rcvbuff.length);
 			
 			while(true)
-			{
+			{				
+				rcvPacket = new DatagramPacket(rcvbuff, rcvbuff.length);
 				try {
 					datagramSocket.receive(rcvPacket);
-					buffer.add(rcvbuff);
-					fos.write(rcvbuff, 0, rcvPacket.getLength());
+					if(rcvPacket.getLength() > 0)
+					{
+						byte[] data = new byte[160];
+						System.arraycopy(rcvbuff, 0, data, 0, rcvPacket.getLength());
+						buffer.add(data);
+						
+						// 访问完后，释放  
+                        semp.release(); 
+					}
+					//Log.i("recv", String.valueOf(buffer.size()));
+					//fos.write(rcvPacket.getData(), 0, rcvPacket.getLength());
 					//cBuffer.putElement(rcvbuff);
 					//Log.i("元素数量", String.valueOf(cBuffer.capacity));
 					//Log.i("收到数据", String.valueOf(rcvPacket.getLength())); 
