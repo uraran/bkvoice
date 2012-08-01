2443d86cf20eb8a147f51e82254479b0ab984e26
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-08-01 16:31:05 +0000

    导入AVD项目

---
+++ trunk/win-x86/VAD/vadtest.c
@@ -0,0 +1,30 @@
+
+#include "stdio.h"
+#include "wb_vad.h"
+void main()
+{	
+		int i,frame=0,temp,vad; 
+		float indata[FRAME_LEN];
+		VadVars *vadstate;					
+		FILE *fp1;
+		fp1=fopen("inls1.wav","rb");
+		wb_vad_init(&(vadstate));			//vad初始化
+		while(!feof(fp1))
+		{	
+			frame++;
+			for(i=0;i<FRAME_LEN;i++)		//读取语音文件
+			{	
+				indata[i]=0;
+				temp=0;
+				fread(&temp,2,1,fp1);
+				indata[i]=(float)temp;
+				if(indata[i]>65535/2)
+				indata[i]=indata[i]-65536;
+			}
+			vad=wb_vad(vadstate,indata);	//进行vad检测
+			printf("%d \n",vad);
+		}
+		printf("ok!");
+		fcloseall();
+		getchar();
+}
