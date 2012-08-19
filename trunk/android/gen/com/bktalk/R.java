201b14bb81a35000ca02a9806375410547fd9542
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-08-19 16:53:48 +0000

    修改中 ...

--- trunk/android/gen/com/bktalk/R.java
+++ trunk/android/gen/com/bktalk/R.java
@@ -14,7 +14,8 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050000;
+        public static final int btnRecv=0x7f050000;
+        public static final int btnSend=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -22,5 +23,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int recv=0x7f040002;
+        public static final int send=0x7f040003;
     }
 }
