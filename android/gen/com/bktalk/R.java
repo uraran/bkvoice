2eed70d0194a9f24b33257315185e7424546f89a
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-08-21 03:19:30 +0000

    调试中，录音出现问题，正在解决 ...

--- android/gen/com/bktalk/R.java
+++ android/gen/com/bktalk/R.java
@@ -11,7 +11,78 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int auto_answer=0x7f020000;
+        public static final int auto_answer_disabled=0x7f020001;
+        public static final int conf=0x7f020002;
+        public static final int configure=0x7f020003;
+        public static final int contacts=0x7f020004;
+        public static final int exit=0x7f020005;
+        public static final int goback=0x7f020006;
+        public static final int home=0x7f020007;
+        public static final int ic_contacts_edit_contacts=0x7f020008;
+        public static final int ic_incall_add=0x7f020009;
+        public static final int ic_incall_answer=0x7f02000a;
+        public static final int ic_incall_end=0x7f02000b;
+        public static final int ic_incall_hangup=0x7f02000c;
+        public static final int ic_incall_ongoing=0x7f02000d;
+        public static final int ic_incall_onhold=0x7f02000e;
+        public static final int ic_launcher_phone=0x7f02000f;
+        public static final int ic_menu_dial_pad=0x7f020010;
+        public static final int ic_menu_end_call=0x7f020011;
+        public static final int ic_search_contacts=0x7f020012;
+        public static final int icon=0x7f020013;
+        public static final int icon22=0x7f020014;
+        public static final int icon32=0x7f020015;
+        public static final int icon64=0x7f020016;
+        public static final int im=0x7f020017;
+        public static final int incall_frame_connected_short=0x7f020018;
+        public static final int incall_frame_connected_tall_land=0x7f020019;
+        public static final int incall_frame_connected_tall_port=0x7f02001a;
+        public static final int incall_frame_ended_short=0x7f02001b;
+        public static final int incall_frame_ended_tall_land=0x7f02001c;
+        public static final int incall_frame_ended_tall_port=0x7f02001d;
+        public static final int incall_frame_hold_short=0x7f02001e;
+        public static final int incall_frame_hold_tall_land=0x7f02001f;
+        public static final int incall_frame_hold_tall_port=0x7f020020;
+        public static final int incall_frame_normal_short=0x7f020021;
+        public static final int incall_frame_normal_tall_land=0x7f020022;
+        public static final int incall_frame_normal_tall_port=0x7f020023;
+        public static final int incall_photo_border=0x7f020024;
+        public static final int mute=0x7f020025;
+        public static final int people=0x7f020026;
+        public static final int picture_busy=0x7f020027;
+        public static final int picture_conference=0x7f020028;
+        public static final int picture_dialing=0x7f020029;
+        public static final int picture_end=0x7f02002a;
+        public static final int picture_frame=0x7f02002b;
+        public static final int picture_unknown=0x7f02002c;
+        public static final int picture_unknown_2=0x7f02002d;
+        public static final int picture_unknown_3=0x7f02002e;
+        public static final int save=0x7f02002f;
+        public static final int stat_sys_phone_call=0x7f020030;
+        public static final int stat_sys_phone_call_bluetooth=0x7f020031;
+        public static final int sym_call=0x7f020032;
+        public static final int sym_call_add_call=0x7f020033;
+        public static final int sym_call_done=0x7f020034;
+        public static final int sym_call_end=0x7f020035;
+        public static final int sym_call_hold_off=0x7f020036;
+        public static final int sym_call_hold_on=0x7f020037;
+        public static final int sym_call_lcd=0x7f020038;
+        public static final int sym_call_merge=0x7f020039;
+        public static final int sym_call_redial=0x7f02003a;
+        public static final int sym_call_speakerphone_off=0x7f02003b;
+        public static final int sym_call_speakerphone_on=0x7f02003c;
+        public static final int sym_fav_five=0x7f02003d;
+        public static final int sym_incoming_call_answer=0x7f02003e;
+        public static final int sym_incoming_call_answer_options=0x7f02003f;
+        public static final int sym_incoming_call_dont_answer=0x7f020040;
+        public static final int sym_incoming_call_hold_answer=0x7f020041;
+        public static final int sym_item_indicator=0x7f020042;
+        public static final int sym_item_indicator_focus=0x7f020043;
+        public static final int sym_presence_available=0x7f020044;
+        public static final int sym_presence_away=0x7f020045;
+        public static final int sym_presence_idle=0x7f020046;
+        public static final int sym_presence_offline=0x7f020047;
     }
     public static final class id {
         public static final int btnRecv=0x7f050000;
@@ -22,8 +93,9 @@ public final class R {
     }
     public static final class string {
         public static final int about=0x7f040004;
+        public static final int about_message=0x7f040005;
         public static final int app_name=0x7f040001;
-        public static final int finish=0x7f040005;
+        public static final int exit=0x7f040006;
         public static final int hello=0x7f040000;
         public static final int recv=0x7f040002;
         public static final int send=0x7f040003;
