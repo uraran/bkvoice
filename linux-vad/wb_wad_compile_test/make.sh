ab4474675d8c1dac2b175fc3fdecfac930b99dd4
dianshijin <dianshijin@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-09-22 08:56:04 +0000

    修改make.sh,test.c. 可以在x86和arm系统编译

--- linux-vad/wb_wad_compile_test/make.sh
+++ linux-vad/wb_wad_compile_test/make.sh
@@ -1,30 +1,15 @@
-gcc -c -I./inc -I../vo-amrwbenc-0.1.2/common/include/ \
+gcc  -o testx86 -I./inc -I./common/include/ \
 src/autocorr.c  src/cor_h_x.c  src/homing.c   src/isp_az.c    src/math_op.c    src/pred_lt4.c  src/random.c    src/util.c \
 src/az_isp.c    src/decim54.c  src/hp400.c    src/isp_isf.c   src/mem_align.c  src/preemph.c   src/residu.c    src/voAMRWBEnc.c \
 src/bits.c      src/deemph.c   src/hp50.c     src/lag_wind.c  src/oper_32b.c   src/q_gain2.c   src/scale.c     src/voicefac.c \
 src/c2t64fx.c   src/dtx.c      src/hp6k.c     src/levinson.c  src/pitch_f4.c   src/qisf_ns.c   src/stream.c    src/wb_vad.c \
 src/c4t64fx.c   src/gpclip.c   src/hp_wsp.c   src/log2.c      src/pit_shrp.c   src/qpisf_2s.c  src/syn_filt.c  src/weight_a.c \
-src/convolve.c  src/g_pitch.c  src/int_lpc.c  src/lp_dec2.c   src/p_med_ol.c   src/q_pulse.c   src/updt_tar.c
+src/convolve.c  src/g_pitch.c  src/int_lpc.c  src/lp_dec2.c   src/p_med_ol.c   src/q_pulse.c   src/updt_tar.c \
+common/cmnMemory.c \
+test.c
 
 
-
-
-
-
-
-
-arm-linux-gcc -c -I./inc -I../vo-amrwbenc-0.1.2/common/include/ -DASM_OPT \
-src/autocorr.c  src/cor_h_x.c  src/homing.c   src/isp_az.c    src/math_op.c    src/pred_lt4.c  src/random.c    src/util.c \
-src/az_isp.c    src/decim54.c  src/hp400.c    src/isp_isf.c   src/mem_align.c  src/preemph.c   src/residu.c    src/voAMRWBEnc.c \
-src/bits.c      src/deemph.c   src/hp50.c     src/lag_wind.c  src/oper_32b.c   src/q_gain2.c   src/scale.c     src/voicefac.c \
-src/c2t64fx.c   src/dtx.c      src/hp6k.c     src/levinson.c  src/pitch_f4.c   src/qisf_ns.c   src/stream.c    src/wb_vad.c \
-src/c4t64fx.c   src/gpclip.c   src/hp_wsp.c   src/log2.c      src/pit_shrp.c   src/qpisf_2s.c  src/syn_filt.c  src/weight_a.c \
-src/convolve.c  src/g_pitch.c  src/int_lpc.c  src/lp_dec2.c   src/p_med_ol.c   src/q_pulse.c   src/updt_tar.c
-
-
-
-
-arm-linux-gcc -o test -I./inc -I./common/include/ -DASM_OPT \
+arm-linux-gcc -o testarm -I./inc -I./common/include/ -DASM_OPT \
 src/autocorr.c  src/cor_h_x.c  src/homing.c   src/isp_az.c    src/math_op.c    src/pred_lt4.c  src/random.c    src/util.c \
 src/az_isp.c    src/decim54.c  src/hp400.c    src/isp_isf.c   src/mem_align.c  src/preemph.c   src/residu.c    src/voAMRWBEnc.c \
 src/bits.c      src/deemph.c   src/hp50.c     src/lag_wind.c  src/oper_32b.c   src/q_gain2.c   src/scale.c     src/voicefac.c \
