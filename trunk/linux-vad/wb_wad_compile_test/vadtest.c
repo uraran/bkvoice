075f153dc7ea9142bdf1945d379545638b032000
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-09-23 02:53:54 +0000

    加入网络数据包发送,准备与vc编写的服务器端对接

--- trunk/linux-vad/wb_wad_compile_test/vadtest.c
+++ trunk/linux-vad/wb_wad_compile_test/vadtest.c
@@ -8,6 +8,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <linux/soundcard.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <string.h>
 
 #define LENGTH 16    /* how many seconds of speech to store */
 #define RATE 16000   /* the sampling rate */
@@ -16,8 +21,9 @@
 
 /* this buffer holds the digitized audio */
 unsigned char buf[LENGTH*RATE*SIZE*CHANNELS/8/1000];
-
-void main()   
+#define SERVERIP      "192.168.2.114"
+#define SERVERPORT    8302 
+int main()   
 {      
     int fd;	/* sound device file descriptor */
     int arg;	/* argument for ioctl calls */
@@ -28,6 +34,20 @@ void main()
     VadVars *vadstate;                     
     FILE *fp1;   
     VO_MEM_OPERATOR voMemoprator;
+    struct sockaddr_in dest_addr;
+
+    int fdsocket = socket(AF_INET, SOCK_DGRAM, 0);    
+    if (fdsocket == -1) 
+    {
+      perror("socket(创建套接字)");
+      return -1;
+    }
+
+    /* 设置远程连接的信息*/
+    dest_addr.sin_family = AF_INET;                 /* 注意主机字节顺序*/
+    dest_addr.sin_port = htons(SERVERPORT);          /* 远程连接端口, 注意网络字节顺序*/
+    dest_addr.sin_addr.s_addr = inet_addr(SERVERIP); /* 远程 IP 地址, inet_addr() 会返回网络字节顺序*/
+    memset(&(dest_addr.sin_zero), 0, 8);                /* 其余结构须置 0*/   
 
     /* open sound device */
     fd = open("/dev/dsp", O_RDWR);
@@ -90,11 +110,20 @@ void main()
         //printf("111111111111111111\n");
         if (status != sizeof(buf))
           perror("read wrong number of bytes");
+
+        status = sendto(fdsocket, buf, sizeof(buf), 0, (struct sockaddr*)&dest_addr, sizeof(struct sockaddr));    
+        if(status == -1)
+        {
+            perror("sendto(数据发送)");
+        }
+
         //printf("2222222222222222222\n");
         vad=wb_vad(vadstate, (short*)buf);    //进行vad检测   
         printf("%d,",vad);   
     }   
     printf("ok!");   
     fcloseall();   
-    getchar();   
+    getchar();  
+
+    return 0; 
 }   
