0958ffd89ee0b8fbed16df30e7513a8060686f4c
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-10-12 12:51:21 +0000

    添加SILK解码，调试中...

--- trunk/linux-vad/speaker1/config.h
+++ trunk/linux-vad/speaker1/config.h
@@ -9,7 +9,7 @@
 #define traceprintf
 #endif
 
-#define BUFFER_COUNT       10   //接收端缓冲区有该数量才允许播放
+#define BUFFER_COUNT       3   //接收端缓冲区有该数量才允许播放
 #define SAMPLERATE     24000 //定义采样率
 #define READMSFORONCE     20 //采样周期(ms)
 
@@ -21,18 +21,21 @@
 #define RECORD_CAPTURE_PCM         0
 #define RECORD_SEND_PCM            0
 #define RECORD_RECV_PCM            0
-#define RECORD_PLAY_PCM            0
+#define RECORD_PLAY_PCM            1
 
 #define MAX_SEND_NO                6
 //char buffer[SAMPLERATE/1000*READMSFORONCE*sizeof(short)];
 struct AudioBuffer;
 typedef struct AudioBuffer
 {
-    unsigned char buffer[SAMPLERATE/1000*READMSFORONCE*sizeof(short)];
+    unsigned char buffer_decode[SAMPLERATE/1000*READMSFORONCE*sizeof(short)];//解码缓冲区
+    int No;//在环形缓冲区中序号
+    char received;//是否接收成功
+    char decoded;//是否已经解码成功
     int FrameNO;//包序号
 	  int time;//timestampe
 	  char SendNO;//
-    char Valid;//包是否有效
+    //char Valid;//包是否有效
     char reserver0;//
     char reserver1;//
     char reserver2;//
@@ -41,7 +44,8 @@ typedef struct AudioBuffer
     char reserver5;//
     struct AudioBuffer  *pPrior;
     struct AudioBuffer  *pNext;
-    int count;//实际有效字节数量
+    int count_recv;//接收数据数量
+    int count_decode;//解码后数据数量
     unsigned char buffer_recv[SAMPLERATE/1000*READMSFORONCE*sizeof(short)];//接收缓冲区
 } __attribute__ ((packed)) AUDIOBUFFER;
 
