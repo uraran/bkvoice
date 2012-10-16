d305fba736f3fe6fdbbdc2b37200d4a4bf1cf219
qq271885846 <qq271885846@ed4c09e6-f5e8-dd9c-d1e1-30c58becc500>
2012-10-16 00:43:11 +0000

    提交build-arm-linux-2.95.3.sh,准备在8120上测试

---
+++ jrtplib/build-arm-linux-2.95.3.sh
@@ -0,0 +1,44 @@
+
+export SDK_DIR=$PWD/install_sdk/arm-linux-2.95.3/
+echo "jrtplib安装路径: $SDK_DIR"
+
+
+#清除include与lib文件夹
+echo "删除 include与lib文件夹"
+rm -rf $PWD/../bin
+rm -rf $PWD/../include
+rm -rf $PWD/../lib
+rm -rf $PWD/../share
+rm -rf $SDK_DIR/bin
+rm -rf $SDK_DIR/include
+rm -rf $SDK_DIR/lib
+
+#编译 jthread-1.2.1
+echo "编译 jthread-1.2.1"
+pushd jthread-1.2.1
+make clean
+make distclean
+chmod +x configure
+./configure --prefix=$SDK_DIR --enable-shared --enable-static --host=arm-linux
+make -j5
+make install
+popd
+
+
+
+
+
+#编译 jrtplib-3.7.1
+echo "编译 jrtplib-3.7.1"
+pushd jrtplib-3.7.1
+make clean
+make distclean
+chmod +x configure
+./configure --prefix=$SDK_DIR --enable-shared --enable-static --host=arm-linux
+make -j5
+make install
+popd
+
+
+
+
